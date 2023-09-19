44f6cc14d280ea21487576f8a8a661fadc209314
jhl221123 <jhl221123@naver.com>
2023-09-19 14:37:57 +0900

    트리거가 push일 경우 merge에도 적용되는지 테스트(#3)

--- src/main/java/practice/githubactions/Test.java
+++ src/main/java/practice/githubactions/Test.java
@@ -2,6 +2,6 @@ package practice.githubactions;
 
 public class Test {
     public void test() {
-        System.out.println("워크플로우 실행 시점 테스트");
+        System.out.println("트리거를 push로 설정하면 merge 할 때도 적용될까?");
     }
 }
