4e767cf1b98cb5dacdd8265641e4c9b72b60a1e0
jhl221123 <jhl221123@naver.com>
2023-09-19 14:49:24 +0900

    원격과 동기화 한 후 push 하면 merge되지 않을까?

--- src/main/java/practice/githubactions/Test.java
+++ src/main/java/practice/githubactions/Test.java
@@ -2,6 +2,6 @@ package practice.githubactions;
 
 public class Test {
     public void test() {
-        System.out.println("트리거를 push로 설정하면 merge가 거부되어도 적용될까?");
+        System.out.println("원격과 동기화한 후 push");
     }
 }
